bc07c6dd26545e6b6457c77091cdf98341c38f9e
Andreas Linnert <andreas@linnertmedia.de>
2020-05-25 14:06:38 +0200

    Remove Node.js files

--- dist/globals.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = require("path");
-const os_1 = require("os");
-exports.globalConfigDirectory = path_1.resolve(os_1.homedir(), '.protofiles');
-exports.configFilename = 'protofile.js';
--- src/globals.ts
+++
@@ -1,5 +0,0 @@
-import { resolve } from 'path'
-import { homedir } from 'os'
-
-export const globalConfigDirectory = resolve(homedir(), '.protofiles')
-export const configFilename = 'protofile.js'
